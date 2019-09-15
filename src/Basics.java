7a6054213cec813ea395610aa2d534829c98d987
tony-albanese <tony.albanese2000@gmail.com>
2019-09-15 13:17:02 +0200

    Initial commit

---
+++ src/Basics.java
@@ -0,0 +1,42 @@
+public class Basics {
+    public static void main(String[] args) {
+        System.out.println("Hell, world!");
+
+        //This is a one line comment.
+
+        /*
+        This is for multiple line comments.
+        The compiler ignores everything here.
+         */
+
+        //Declare a variable and use it.
+
+        int x = 2;
+        System.out.println(x);
+
+        x = 34; //Change the value.
+        System.out.println(x);
+
+        float y = 5.044f;
+        double z = 4.24345345345345;
+        long longInteger = 2342342382882388238L;
+
+        boolean fishSoupIsTasty = false;
+
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(longInteger);
+        System.out.println(fishSoupIsTasty);
+
+        String message = "Greetings from G11 Computer Science";
+        System.out.println(message);
+
+        int a = 7;
+        int b = 199;
+        boolean testBoolean = a > b;
+
+        System.out.println(testBoolean);
+
+        
+    }
+}
