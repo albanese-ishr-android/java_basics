260d28cfcf70241a53120aedc78cee1480a4b371
tony-albanese <tony.albanese2000@gmail.com>
2019-09-21 09:27:55 +0200

    Added TODO 4 and 5

--- src/BasicsFormativeAssessment.java
+++ src/BasicsFormativeAssessment.java
@@ -1,6 +1,13 @@
 public class BasicsFormativeAssessment {
 
-    
+    /*
+    I've declared a main() function for you. This is the function that the Java interpreter uses to start the programs. You
+    don't need to put anything in here. I will run your programs to test the output.
+     */
+    public static void main(String[] args) {
+        usingVariables();
+
+    }
     /*
     Please complete the TODOs in this file. I will use this to assess your basic Java Skills.
      */
@@ -12,5 +19,18 @@ public class BasicsFormativeAssessment {
 
         //TODO (2) -- Use System.out.println() to print the values of the variables.
 
+    }
+
+    public static void goingLoopy() {
+        /*
+        This is to check your ability to write for and while loops.
+         */
+
+        //TODO (3) - Write a for loop that will print the first 25 multiples of 2.
+        //The output should be 2, 4, 6, 8 ... 50
+
+
+        //TODO (4) - Fix the while loop
+        
     }
 }
