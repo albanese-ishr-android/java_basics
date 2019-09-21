861d4893255ccfcc33c8d3f0c75a68ffb273b7b3
tony-albanese <tony.albanese2000@gmail.com>
2019-09-21 09:21:38 +0200

    Added some TODOs.

--- src/BasicsFormativeAssessment.java
+++ src/BasicsFormativeAssessment.java
@@ -1,2 +1,16 @@
 public class BasicsFormativeAssessment {
+
+    
+    /*
+    Please complete the TODOs in this file. I will use this to assess your basic Java Skills.
+     */
+
+
+    public static void usingVariables() {
+        //TODO (1) -- Declare three variables and initialize them with a value. One variable should be a string, a float, a boolean, and an integer.
+
+
+        //TODO (2) -- Use System.out.println() to print the values of the variables.
+
+    }
 }
