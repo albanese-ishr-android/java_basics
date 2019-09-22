af3f1b4c163e3ada601742b26cb0350f50a9de28
tony-albanese <tony.albanese2000@gmail.com>
2019-09-22 15:18:00 +0200

    Initial commit.

---
+++ src/PseudocodeTranslations.java
@@ -0,0 +1,21 @@
+public class PseudocodeTranslations {
+
+    public static void main(String[] args) {
+
+        sumOdd(-3);
+        sumOdd(16);
+    }
+
+
+    public static void sumOdd(int n) {
+
+        //The input is given here as n. You can use the variable n in this algorithm. You don't neet to set its value.
+
+        System.out.println(n); //Print the input the value.
+
+        //HINT: How can you generate an odd number?
+
+
+    }
+
+}
