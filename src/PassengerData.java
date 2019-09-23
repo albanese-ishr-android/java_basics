6369637fae4cae06ffc1626976aeb6eabe52de29
tony-albanese <tony.albanese2000@gmail.com>
2019-09-23 14:01:11 +0200

    Initial commit.

---
+++ src/PassengerData.java
@@ -0,0 +1,21 @@
+public class PassengerData {
+    public static int[] p_array = {221, 71, 81, 215, 721, 812, 95, 12, 15, 36, 11, 50, 121, 211,
+            356, 311 };
+
+
+    public static void main(String[] args) {
+        //Test
+        //Get the data for all of the mondays.
+
+        //mondays = p_array[0], p_array[7], p_array[14]
+        //tuesdays = p_array[1], p_array[8], p_array[15]
+        //wednesdays = p_array[2], p_array[9], p_array[16]
+
+
+
+
+    }
+
+
+
+}
