1ad244d86c1f3e75298fa77caa056e2fe03d0acd
tony-albanese <tony.albanese2000@gmail.com>
2019-09-24 18:47:15 +0200

    Refactored name to be more descriptive.

--- src/PassangerArrayHL.java
+++ src/PassangerArrayHL.java
@@ -4,16 +4,14 @@ public class PassangerArrayHL {
 
     static int[][] twoDimensionalArray = new int[30][7];
     static int[] passengers = new int[210];
-    
+
     public static void main(String[] args) {
-        passengerData();
+        generatePassengerData(passengers);
+        sortPassengerTwoDimensionalArray();
     }
 
 
-    public static void passengerData() {
-
-
-        generatePassengerData(passengers);
+    public static void sortPassengerTwoDimensionalArray() {
         int currentPassengerArrayIndex = 0;
 
         //This block of code sorts the data into a two dimensional array : 30 weeks as rows and 7 columns as days.
@@ -26,7 +24,6 @@ public class PassangerArrayHL {
             currentPassengerArrayIndex = currentPassengerArrayIndex + 1;
             System.out.println("Week: " + currentWeek + "\tDay: " + currentDay + "\tPassengers: " + twoDimensionalArray[currentWeek][currentDay]);
         }
-
 
     }
 
